30a76f617ccef653464cb0b75ac1b3a8100b0e08
rtharayil <ranjiththarayil@gmail.com>
2018-02-23 10:19:40 +0530

    Add files via upload
    modified version , removed test suite

---
+++ Java/src/main/java/com/hamarabasket/QualityRuleEngine.java
@@ -0,0 +1,62 @@
+package com.hamarabasket;
+
+class QualityRuleEngine {
+    Item[] items;
+
+    public QualityRuleEngine(Item[] items) {
+        this.items = items;
+    }
+
+    public void updateQuality() {
+        for (int i = 0; i < items.length; i++) {
+            if (!items[i].name.equals("Indian Wine")
+                    && !items[i].name.equals("Movie Tickets")) {
+                if (items[i].quality > 0) {
+                    if (!items[i].name.equals("Forest Honey")) {
+                        items[i].quality = items[i].quality - 1;
+                    }
+                }
+            } else {
+                if (items[i].quality < 50) {
+                    items[i].quality = items[i].quality + 1;
+
+                    if (items[i].name.equals("Movie Tickets")) {
+                        if (items[i].sellIn < 11) {
+                            if (items[i].quality < 50) {
+                                items[i].quality = items[i].quality + 1;
+                            }
+                        }
+
+                        if (items[i].sellIn < 6) {
+                            if (items[i].quality < 50) {
+                                items[i].quality = items[i].quality + 1;
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (!items[i].name.equals("Forest Honey")) {
+                items[i].sellIn = items[i].sellIn - 1;
+            }
+
+            if (items[i].sellIn < 0) {
+                if (!items[i].name.equals("Indian Wine")) {
+                    if (!items[i].name.equals("Movie Tickets")) {
+                        if (items[i].quality > 0) {
+                            if (!items[i].name.equals("Forest Honey")) {
+                                items[i].quality = items[i].quality - 1;
+                            }
+                        }
+                    } else {
+                        items[i].quality = items[i].quality - items[i].quality;
+                    }
+                } else {
+                    if (items[i].quality < 50) {
+                        items[i].quality = items[i].quality + 1;
+                    }
+                }
+            }
+        }
+    }
+}
